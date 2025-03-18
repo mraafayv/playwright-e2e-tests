d556b6eff2bee9493ddb9cabd76185c848f9e868
Muhammad Raafay <raafayvohra.mrv@gmail.com>
2025-03-18 21:06:04 +0100

    add Test 3 "Test the logout functionality of the application"

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -11,22 +11,41 @@ test.describe("Authentication Tests", () => {
     await page.locator("#password-input").fill("supersecure");
     await page.getByRole("button", { name: "Login" }).click();
 
-    await expect(page).toHaveURL("/");
+    await expect(page).toHaveURL("http://localhost:3000/");
     await expect(page.locator("h1")).toContainText("Home");
   });
 
   test("User login with invalid credentials should show error", async ({
     page,
   }) => {
-    await page.goto("/login");
+    await page.goto("http://localhost:3000/login");
 
     await page.locator("#email-input").fill("invalid_user@maddox.ai");
     await page.locator("#password-input").fill("wrongpassword");
     await page.getByRole("button", { name: "Login" }).click();
 
-    await expect(page).toHaveURL("/login");
+    await expect(page).toHaveURL("http://localhost:3000/login");
     await expect(page.getByRole("paragraph")).toContainText(
       "Invalid email or password. Try again."
     );
   });
 });
+
+
+//Test 3
+test("User should logout successfully", async ({ page }) => {
+    await page.goto("http://localhost:3000/login");
+  
+    await page.locator("#email-input").fill("test@maddox123.ai");
+    await page.locator("#password-input").fill("supersecure");
+    await page.getByRole("button", { name: "Login" }).click();
+  
+    await expect(page.getByRole("button", { name: "Logout" })).toBeVisible();
+    
+    await page.getByRole("button", { name: "Logout" }).click();
+    
+    await expect(page.getByRole("heading", { name: "Demo Login Form" })).toBeVisible();
+    await page.goto("http://localhost:3000/");
+    await expect(page).toHaveURL("http://localhost:3000/login");
+  });
+  
