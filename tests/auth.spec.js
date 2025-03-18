7ce98c9c6bbde61b6a8bb4edd219803bc8b71220
Muhammad Raafay <raafayvohra.mrv@gmail.com>
2025-03-18 20:56:18 +0100

    add Test 1 "Test the login flow of the application"

---
+++ tests/auth.spec.js
@@ -0,0 +1,32 @@
+// @ts-check
+
+import { test, expect } from "@playwright/test";
+
+//Test 1
+test.describe("Authentication Tests", () => {
+  test("User should login with valid credentials", async ({ page }) => {
+    await page.goto("http://localhost:3000/login");
+
+    await page.locator("#email-input").fill("test@maddox123.ai");
+    await page.locator("#password-input").fill("supersecure");
+    await page.getByRole("button", { name: "Login" }).click();
+
+    await expect(page).toHaveURL("/");
+    await expect(page.locator("h1")).toContainText("Home");
+  });
+
+  test("User login with invalid credentials should show error", async ({
+    page,
+  }) => {
+    await page.goto("/login");
+
+    await page.locator("#email-input").fill("invalid_user@maddox.ai");
+    await page.locator("#password-input").fill("wrongpassword");
+    await page.getByRole("button", { name: "Login" }).click();
+
+    await expect(page).toHaveURL("/login");
+    await expect(page.getByRole("paragraph")).toContainText(
+      "Invalid email or password. Try again."
+    );
+  });
+});
