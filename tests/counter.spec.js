fb15d131d9a5485e5a414fc6eca562f104db4ee6
Muhammad Raafay <raafayvohra.mrv@gmail.com>
2025-03-18 21:05:39 +0100

    add Test 2 "Test the counter functionality of the application"

---
+++ tests/counter.spec.js
@@ -0,0 +1,27 @@
+// @ts-check
+
+import { test, expect } from "@playwright/test";
+
+// Test 2
+test('test', async ({ page }) => {
+    await page.goto('http://localhost:3000/login');
+   
+    await page.locator('#email-input').fill('test@maddox123.ai');
+    await page.locator('#password-input').fill('supersecure');
+    await page.getByRole('button', { name: 'Login' }).click();
+   
+    await expect(page.getByRole('paragraph')).toContainText('0');
+
+    await page.getByRole('button', { name: '+' }).click();
+    await expect(page.getByRole('paragraph')).toContainText('1');
+
+    await page.getByRole('button', { name: '-' }).click();
+    await expect(page.getByRole('paragraph')).toContainText('0');
+
+    await page.getByRole('button', { name: '+' }).click();
+    await page.getByRole('button', { name: '+' }).click();
+    await expect(page.getByRole('paragraph')).toContainText('2');
+
+    await page.getByRole('button', { name: 'Reset' }).click();
+    await expect(page.getByRole('paragraph')).toContainText('0');
+  });
