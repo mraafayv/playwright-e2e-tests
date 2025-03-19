9d811c1381f04b6694afa4b89611ed3d2405e5f1
Muhammad Raafay <raafayvohra.mrv@gmail.com>
2025-03-19 05:18:00 +0100

    use baseUrl in auth.spec

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -5,26 +5,26 @@ import { test, expect } from "@playwright/test";
 //Test 1
 test.describe("Authentication Tests", () => {
   test("User should login with valid credentials", async ({ page }) => {
-    await page.goto("http://localhost:3000/login");
+    await page.goto("/login");
 
     await page.locator("#email-input").fill("test@maddox123.ai");
     await page.locator("#password-input").fill("supersecure");
     await page.getByRole("button", { name: "Login" }).click();
 
-    await expect(page).toHaveURL("http://localhost:3000/");
+    await expect(page).toHaveURL("/");
     await expect(page.locator("h1")).toContainText("Home");
   });
 
   test("User login with invalid credentials should show error", async ({
     page,
   }) => {
-    await page.goto("http://localhost:3000/login");
+    await page.goto("/login");
 
     await page.locator("#email-input").fill("invalid_user@maddox.ai");
     await page.locator("#password-input").fill("wrongpassword");
     await page.getByRole("button", { name: "Login" }).click();
 
-    await expect(page).toHaveURL("http://localhost:3000/login");
+    await expect(page).toHaveURL("/login");
     await expect(page.getByRole("paragraph")).toContainText(
       "Invalid email or password. Try again."
     );
@@ -34,7 +34,7 @@ test.describe("Authentication Tests", () => {
 
 //Test 3
 test("User should logout successfully", async ({ page }) => {
-    await page.goto("http://localhost:3000/login");
+    await page.goto("/login");
   
     await page.locator("#email-input").fill("test@maddox123.ai");
     await page.locator("#password-input").fill("supersecure");
@@ -45,7 +45,7 @@ test("User should logout successfully", async ({ page }) => {
     await page.getByRole("button", { name: "Logout" }).click();
     
     await expect(page.getByRole("heading", { name: "Demo Login Form" })).toBeVisible();
-    await page.goto("http://localhost:3000/");
-    await expect(page).toHaveURL("http://localhost:3000/login");
+    await page.goto("/");
+    await expect(page).toHaveURL("/login");
   });
   
