a225e22f77819b8495c793c3050a9e66ce1fd759
Muhammad Raafay <raafayvohra.mrv@gmail.com>
2025-03-19 08:18:34 +0100

    add redirection edge case

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -30,27 +30,27 @@ test.describe("Authentication Tests", () => {
     );
   });
 
-  //edge cases
-//   test("Empty fields should show validation error", async ({ page }) => {
-//     await page.goto("/login");
+  //login edge cases
+  //   test("Empty fields should show validation error", async ({ page }) => {
+  //     await page.goto("/login");
 
-//     await page.getByRole("button", { name: "Login" }).click();
+  //     await page.getByRole("button", { name: "Login" }).click();
 
-//     await expect(page.getByRole("paragraph")).toContainText(
-//         "Fields cannot be empty"
-//       );
-//   });
+  //     await expect(page.getByRole("paragraph")).toContainText(
+  //         "Fields cannot be empty"
+  //       );
+  //   });
 
-//   test("SQL injection attempt should not work", async ({ page }) => {
-//     await page.goto("/login");
-//     await page.locator("#email-input").fill("admin");
-//     await page.locator("#password-input").fill("' OR 1=1 --");
-//     await page.getByRole("button", { name: "Login" }).click();
+  //   test("SQL injection attempt should not work", async ({ page }) => {
+  //     await page.goto("/login");
+  //     await page.locator("#email-input").fill("admin");
+  //     await page.locator("#password-input").fill("' OR 1=1 --");
+  //     await page.getByRole("button", { name: "Login" }).click();
 
-//     await expect(page.getByRole("paragraph")).toContainText(
-//         "Invalid credentials"
-//       );
-//   });
+  //     await expect(page.getByRole("paragraph")).toContainText(
+  //         "Invalid credentials"
+  //       );
+  //   });
 });
 
 //Test 3
@@ -71,3 +71,18 @@ test("User should logout successfully", async ({ page }) => {
   await page.goto("/");
   await expect(page).toHaveURL("/login");
 });
+
+//redirection edge case
+// test("User must redirect to home when accessing login page while logged in", async ({
+//   page,
+// }) => {
+//   await page.goto("/login");
+
+//   await page.locator("#email-input").fill("test@maddox123.ai");
+//   await page.locator("#password-input").fill("supersecure");
+//   await page.getByRole("button", { name: "Login" }).click();
+
+//   await page.goto("/login");
+
+//   await expect(page).toHaveURL("/");
+// });
