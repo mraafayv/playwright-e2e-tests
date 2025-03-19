e99996e5318961bad97b448a542a15613ece7ca4
Muhammad Raafay <raafayvohra.mrv@gmail.com>
2025-03-19 08:04:58 +0100

    add an edge case to check counter must not go below zero

--- tests/counter.spec.js
+++ tests/counter.spec.js
@@ -3,25 +3,45 @@
 import { test, expect } from "@playwright/test";
 
 // Test 2
-test('Counter should increment, decrement, and reset correctly', async ({ page }) => {
-    await page.goto('/login');
-   
-    await page.locator('#email-input').fill('test@maddox123.ai');
-    await page.locator('#password-input').fill('supersecure');
-    await page.getByRole('button', { name: 'Login' }).click();
-   
-    await expect(page.getByRole('paragraph')).toContainText('0');
+test.describe("Counter Functionality Tests", () => {
+  test("Counter should increment, decrement, and reset correctly", async ({
+    page,
+  }) => {
+    await page.goto("/login");
 
-    await page.getByRole('button', { name: '+' }).click();
-    await expect(page.getByRole('paragraph')).toContainText('1');
+    await page.locator("#email-input").fill("test@maddox123.ai");
+    await page.locator("#password-input").fill("supersecure");
+    await page.getByRole("button", { name: "Login" }).click();
 
-    await page.getByRole('button', { name: '-' }).click();
-    await expect(page.getByRole('paragraph')).toContainText('0');
+    await expect(page.getByRole("paragraph")).toContainText("0");
 
-    await page.getByRole('button', { name: '+' }).click();
-    await page.getByRole('button', { name: '+' }).click();
-    await expect(page.getByRole('paragraph')).toContainText('2');
+    await page.getByRole("button", { name: "+" }).click();
+    await expect(page.getByRole("paragraph")).toContainText("1");
 
-    await page.getByRole('button', { name: 'Reset' }).click();
-    await expect(page.getByRole('paragraph')).toContainText('0');
+    await page.getByRole("button", { name: "-" }).click();
+    await expect(page.getByRole("paragraph")).toContainText("0");
+
+    await page.getByRole("button", { name: "+" }).click();
+    await page.getByRole("button", { name: "+" }).click();
+    await expect(page.getByRole("paragraph")).toContainText("2");
+
+    await page.getByRole("button", { name: "Reset" }).click();
+    await expect(page.getByRole("paragraph")).toContainText("0");
   });
+
+  //edge case
+  //   test("Counter should not go below zero", async ({ page }) => {
+  //     await page.goto("/login");
+
+  //     await page.locator("#email-input").fill("test@maddox123.ai");
+  //     await page.locator("#password-input").fill("supersecure");
+  //     await page.getByRole("button", { name: "Login" }).click();
+
+  //     await expect(page.getByRole("paragraph")).toContainText("0");
+
+  //     await page.getByRole("button", { name: "-" }).click();
+
+  //     await expect(page.getByRole("button", { name: "-" })).toBeDisabled();
+  //     await expect(page.getByRole("paragraph")).toContainText("0");
+  //   });
+});
